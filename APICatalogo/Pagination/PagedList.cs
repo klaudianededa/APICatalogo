f262728a31d6e44da3947f1336547a40121dc19e
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-03 14:56:25 -0300

    implementa paginação assíncrona

--- APICatalogo/Pagination/PagedList.cs
+++ APICatalogo/Pagination/PagedList.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Pagination;
 
-public class PagedList<T> : List<T> where T : class
+public class PagedList<T>  : List<T> where T : class
 {
-    public int CurrentPage { get; set; }
-    public int TotalPages { get; set; }
-    public int PageSize { get; set; }
-    public int TotalCount { get; set; }
+    public int CurrentPage { get; private set; }
+    public int TotalPages { get; private set; }
+    public int PageSize { get; private set; }
+    public int TotalCount { get; private set; }
 
     public bool HasPrevious => CurrentPage > 1;
     public bool HasNext => CurrentPage < TotalPages;
@@ -26,10 +22,14 @@ public class PagedList<T> : List<T> where T : class
         AddRange(items);
     }
 
-    public static PagedList<T> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
+    public static PagedList<T> ToPagedList(IQueryable<T> source, 
+                                                       int pageNumber,
+                                                       int pageSize)
     {
         var count = source.Count();
-        var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+        var items = source.Skip((pageNumber - 1) * pageSize)
+                                .Take(pageSize)
+                                .ToList();
 
         return new PagedList<T>(items, count, pageNumber, pageSize);
     }
--- APICatalogo/Repositories/ProdutoRepository.cs
+++ APICatalogo/Repositories/ProdutoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
 using APICatalogo.Pagination;
+using X.PagedList;
 
 namespace APICatalogo.Repositories;
 
@@ -9,46 +10,54 @@ public class ProdutoRepository : Repository<Produto>, IProdutoRepository
     public ProdutoRepository(AppDbContext context) : base(context)
     {}
 
-    public async Task<PagedList<Produto>> GetProdutosAsync(ProdutosParameters produtosParams)
+    public async Task<IPagedList<Produto>> GetProdutosAsync(ProdutosParameters produtosParams)
     {
         var produtos = await GetAllAsync();
+
         var produtosOrdenados = produtos.OrderBy(p => p.ProdutoId).AsQueryable();
 
-        var resultado = PagedList<Produto>.ToPagedList(produtosOrdenados,
-            produtosParams.PageNumber, produtosParams.PageSize);
+        var resultado = await produtosOrdenados.ToPagedListAsync(produtosParams.PageNumber, 
+                                                           produtosParams.PageSize);        
 
         return resultado;
     }
 
-    public async Task<PagedList<Produto>> GetProdutosFiltroPrecoAsync(ProdutosFiltroPreco produtosFiltroParams)
+    public async Task<IPagedList<Produto>> GetProdutosFiltroPrecoAsync(ProdutosFiltroPreco produtosFiltroParams)
     {
         var produtos = await GetAllAsync();
 
-        if (produtosFiltroParams.Preco.HasValue && !string.IsNullOrEmpty(produtosFiltroParams.PrecoCriterio))
+        if (produtosFiltroParams.Preco.HasValue && !
+            string.IsNullOrEmpty(produtosFiltroParams.PrecoCriterio))
         {
-            if (produtosFiltroParams.PrecoCriterio.Equals("maior", StringComparison.OrdinalIgnoreCase))
+            if (produtosFiltroParams.PrecoCriterio.Equals("maior", 
+                               StringComparison.OrdinalIgnoreCase))
             {
-                produtos = produtos.Where(p => p.Preco > produtosFiltroParams.Preco.Value).OrderBy(p => p.Preco);
+                produtos = produtos.Where(p => p.Preco > produtosFiltroParams.Preco.Value)
+                                                                    .OrderBy(p => p.Preco);
             }
-            else if (produtosFiltroParams.PrecoCriterio.Equals("menor", StringComparison.OrdinalIgnoreCase))
+            else if (produtosFiltroParams.PrecoCriterio.Equals("menor", 
+                                    StringComparison.OrdinalIgnoreCase))
             {
-                produtos = produtos.Where(p => p.Preco < produtosFiltroParams.Preco.Value).OrderBy(p => p.Preco);
+                produtos = produtos.Where(p => p.Preco < produtosFiltroParams.Preco.Value)
+                                                                   .OrderBy(p => p.Preco);
             }
-            else if (produtosFiltroParams.PrecoCriterio.Equals("igual", StringComparison.OrdinalIgnoreCase))
+            else if (produtosFiltroParams.PrecoCriterio.Equals("igual", 
+                                     StringComparison.OrdinalIgnoreCase))
             {
-                produtos = produtos.Where(p => p.Preco == produtosFiltroParams.Preco.Value).OrderBy(p => p.Preco);
+                produtos = produtos.Where(p => p.Preco == produtosFiltroParams.Preco.Value)
+                                                                     .OrderBy(p => p.Preco);
             }
         }
-        var produtosFiltrados = PagedList<Produto>.ToPagedList(produtos.AsQueryable(), produtosFiltroParams.PageNumber,
-                                                                                              produtosFiltroParams.PageSize);
+
+        var produtosFiltrados = await produtos.ToPagedListAsync(produtosFiltroParams.PageNumber,
+                                                          produtosFiltroParams.PageSize);
         return produtosFiltrados;
     }
 
     public async Task<IEnumerable<Produto>> GetProdutosPorCategoriaAsync(int id)
     {
         var produtos = await GetAllAsync();
-        var produtosCategoria = produtos.Where(p => p.CategoriaId == id);
-
+        var produtosCategoria = produtos.Where(c => c.CategoriaId == id);
         return produtosCategoria;
     }
 }
